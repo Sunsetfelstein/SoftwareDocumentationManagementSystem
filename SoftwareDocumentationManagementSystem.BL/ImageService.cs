45d335fd2acc76cd1679f6c81d537fd78ecb56d4
sunsetfelstein <robinbobinbarabeck@yandex.ru>
2024-04-07 18:48:10 +0500

    Добавлен сервис для создания картинок

---
+++ SoftwareDocumentationManagementSystem.BL/ImageService.cs
@@ -0,0 +1,30 @@
+﻿using SoftwareDocumentationManagementSystem.Core.Models;
+using CSharpFunctionalExtensions;
+using Microsoft.AspNetCore.Http;
+
+namespace SoftwareDocumentationManagementSystem.BL;
+
+public class ImageService
+{
+    public Result<Image> CreateImage(IFormFile formFile, Guid softwareId, Software software, Guid authorId, User author, string description)
+    {
+        try
+        {
+            byte[] imageData = null;
+            
+            using(var binaryReader = new BinaryReader(formFile.OpenReadStream()))
+            {
+                imageData = binaryReader.ReadBytes((int)formFile.Length);
+            }
+
+            var imageResult = Image.Create(Guid.NewGuid(), softwareId, software, authorId, author, description,
+                imageData);
+
+            return imageResult;
+        }
+        catch (Exception exception)
+        {
+            return Result.Failure<Image>(exception.Message);
+        }
+    }
+}
